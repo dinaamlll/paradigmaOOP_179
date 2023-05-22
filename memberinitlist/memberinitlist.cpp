80c45fe646e4a3cd36225c9e64983a715b90f2b4
dinaamlll <dinamaliaa17@gmail.com>
2023-05-22 15:40:08 +0700

    public class anak

---
+++ memberinitlist/memberinitlist.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) { // Member Ini
+		// Definsi
+	}
+
+	~mahasiswa() {
+		cout << "Id = " << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+
+	}
+};
